1e7ee1d69d9126741356aaae6ecae6110f52ddba
Vandersoom <vanderson.anjos123@gmail.com>
2025-09-04 11:29:12 -0400

    anotacao uso ModelViewset

--- backend/terceiros/urls.py
+++ backend/terceiros/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from .views import TerceirosView
 
+router = DefaultRouter()
+router.register(r"terceiros", TerceirosView)
+
 urlpatterns = [
-    path('terceiros/', TerceirosView.as_view(), name='terceiros'),
+    path("", include(router.urls)),
 ]
--- backend/terceiros/views.py
+++ backend/terceiros/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import viewsets
 from .models import Terceiro
 from .serialazer import TerceiroSerializer
+
 # Create your views here.
 
-class TerceirosView(APIView):
-    def get(self, request):
-        terceiros = Terceiro.objects.all()
-        serializer = TerceiroSerializer(terceiros, many=True)
-        return Response(serializer.data)
-        
 
+class TerceirosView(viewsets.ModelViewSet):
+    queryset = Terceiro.objects.all()
+    serializer_class = TerceiroSerializer
 
+    
