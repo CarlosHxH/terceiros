721f201acdad3518f0e645cd1ab561b6359c5a11
Vandersoom <vanderson.anjos123@gmail.com>
2025-09-13 11:21:53 -0400

    renomeanado ulrs documentacao endpoints -  api/docs e api/redoc

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -33,6 +33,6 @@ urlpatterns = [
     path('api/', include('relatorios.urls')),
 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
